6ff71a8279395a9e10962d2d23c14e1fd4785f92
SonoilSindaco <giovanni.conidi03@gmail.com>
2025-01-14 19:46:54 +0100

    Work in progress

--- src/main/java/org/example/teammanagement/LoginController.java
+++ src/main/java/org/example/teammanagement/LoginController.java
@@ -1,10 +1,12 @@
 package org.example.teammanagement;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Cursor;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
@@ -17,6 +19,7 @@ import java.io.IOException;
 public class LoginController {
     public TextField usernameField;
     public PasswordField passwordField;
+    public Button LogInButton;
     Model model = new Model();
     private final Alert errorAlert = new Alert(Alert.AlertType.ERROR);
 
@@ -28,34 +31,41 @@ public class LoginController {
         ((javafx.scene.Node) mouseEvent.getSource()).setCursor(Cursor.DEFAULT);
     }
 
-    public void send(KeyEvent event) {
-        if(event.getCode().equals(KeyCode.ENTER)){
-            switch (model.authenticateUser(usernameField.getText(), passwordField.getText())){
-                case -1:
-                    errorAlert.setTitle("ERRORE");
-                    errorAlert.setHeaderText("ERRORE!!!");
-                    errorAlert.setContentText("UTENTE NON TROVATO!");
-                    errorAlert.show();
-                case 0: //ROBA ADMIN
-                case 1:
-                    try {
-                        FXMLLoader loader = new FXMLLoader(getClass().getResource("LineUp.fxml"));
-                        Parent root = loader.load();
-                        Stage stage = (Stage) usernameField.getScene().getWindow();
-                        stage.setWidth(630);
-                        Scene scene = new Scene(root);
-                        stage.setTitle(usernameField.getText());
-                        stage.setScene(scene);
-                        stage.show();
+    public void send() {
+        if(model.authenticateUser(usernameField.getText(), passwordField.getText()) == -1){
+                errorAlert.setTitle("ERRORE");
+                errorAlert.setHeaderText("ERRORE!!!");
+                errorAlert.setContentText("UTENTE NON TROVATO!");
+                errorAlert.show();
+        }
+        else if(model.authenticateUser(usernameField.getText(), passwordField.getText()) == 0){} //ROBA ADMIN
+        else if(model.authenticateUser(usernameField.getText(), passwordField.getText()) == 1) {
+            try {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("LineUp.fxml"));
+                Parent root = loader.load();
+                Stage stage = (Stage) usernameField.getScene().getWindow();
+                stage.setWidth(630);
+                Scene scene = new Scene(root);
+                stage.setTitle(usernameField.getText());
+                stage.setScene(scene);
+                stage.show();
 
-                    }
-                    catch (IOException e) {
+            } catch (IOException e) {
 
-                    }
-                case 2: //ROBA GIOCATORE
             }
         }
-
+        else if(model.authenticateUser(usernameField.getText(), passwordField.getText()) == 2) {//ROBA GIOCATORE
+            }
     }
+    public void sendWithEnter(KeyEvent event){
+        if(event.getCode().equals(KeyCode.ENTER)){
+            send();
+        }
+    }
+
+    public void sendWithClick(ActionEvent event){
+        send();
+    }
+
 
 }
